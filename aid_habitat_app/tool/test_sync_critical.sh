e4b36d7b14b2fb1a0668180a690ebff4c257c7e3
jorisaidhabitat-alt <joris.balluais@gmail.com>
2026-09-15 16:06:15 +0200

    Prepare web hotfix 26 with ownership regression gate

--- aid_habitat_app/tool/test_sync_critical.sh
+++ aid_habitat_app/tool/test_sync_critical.sh
@@ -16,6 +16,7 @@ bash "$script_dir/test_safely.sh" \
   test/services/sync_push_outcome_test.dart \
   test/services/sync_session_scope_test.dart \
   test/services/agent3_sync_operation_ownership_test.dart \
+  test/services/sync_ownership_diagnostic_test.dart \
   test/services/agent4_ownership_migration_test.dart \
   test/services/web_vault_migration_test.dart \
   test/services/agent4_offline_identity_test.dart \
